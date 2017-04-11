ab84df7747340634a44dd7a75c37c5b274bddfad
Abraao Lacerda <abraaobarros3@gmail.com>
2017-04-10 21:23:30 -0300

    Heatmap render Fixed

--- public/js/map.js
+++ public/js/map.js
@@ -2,8 +2,109 @@
 var heatLayer;
 
 
-function render_map(map, day){
+var MapStyle = (function(map){
+	var map = map;
+	function highlightFeature(e) {
 
+	    var layer = e.target;
+	    layer.setStyle({
+	        weight: 5,
+	        color: '#666',
+	        dashArray: '',
+	        fillOpacity: 0.7
+	    });
+
+	    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+	        layer.bringToFront();
+	    }
+	}
+	function resetHighlight(e) {
+	    geojson.resetStyle(e.target);
+	}
+	function zoomToFeature(e) {
+	    map.fitBounds(e.target.getBounds());
+	}
+	
+
+	return {
+		style:function(feature){
+			return {
+		        fillColor: 'white',
+		        weight: 2,
+		        opacity: 1,
+		        color: 'white',
+		        dashArray: '3',
+		        fillOpacity: 0.3
+		    };
+		},
+		onEachFeature: function(feature, layer) {
+		    layer.on({
+		        mouseover: highlightFeature,
+		        mouseout: resetHighlight,
+		        click: zoomToFeature
+		    });
+		}
+
+
+	}
+
+});
+
+
+// Elementos para alimentar o mapa
+
+var MapControl = (function(map){
+	var dayIdx = -1;
+	function actual(){
+		console.log("Renderizando o dia "+dates[--dayIdx])
+		return dates[++dayIdx];
+	}
+
+	return{
+		init: function(){
+			var dayIdx = 0;
+		},
+		next: function() {
+			return dates[++dayIdx];
+		},
+		previous: function(){
+			return dates[--dayIdx];
+		},
+		//retorna os pins das unidades de saúde
+		pins: function(callback){
+
+			Papa.parse("csv/data_20170101.csv",
+				{
+					quotes: false,
+					download:true,
+					delimiter: ";",
+					header: false,
+					complete:callback,
+					newline: "\n"
+				}
+			);
+		},
+
+		heatpoints: function(callback) {
+			Papa.parse("csv/data_"+actual()+".csv",
+				{
+					quotes: false,
+					download:true,
+					delimiter: ";",
+					header: false,
+					complete:callback,
+					newline: "\n"
+				}
+			);
+		}
+	}
+
+	function init(){
+
+	}
+});
+
+function render_map(map){
 		
 		map.eachLayer(function (layer) {
 		    map.removeLayer(layer);
@@ -14,194 +115,35 @@ function render_map(map, day){
 		    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
 		}).addTo(map);
 
-
-	
-		// method that we will use to update the control based on feature properties passed
-
-		function getColor(d) {
-			return 'white';
-		    return d > 1000 ? '#800026' :
-		           d > 500  ? '#BD0026' :
-		           d > 200  ? '#E31A1C' :
-		           d > 100  ? '#FC4E2A' :
-		           d > 50   ? '#FD8D3C' :
-		           d > 20   ? '#FEB24C' :
-		           d > 10   ? '#FED976' :
-		                      '#FFEDA0';
-		}
-
-		function style(feature) {
-		    return {
-		        fillColor: getColor(feature.properties.density),
-		        weight: 2,
-		        opacity: 1,
-		        color: 'white',
-		        dashArray: '3',
-		        fillOpacity: 0.3
-		    };
-		}
-
-		function highlightFeature(e) {
-
-		    var layer = e.target;
-		    layer.setStyle({
-		        weight: 5,
-		        color: '#666',
-		        dashArray: '',
-		        fillOpacity: 0.7
-		    });
-
-		    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-		        layer.bringToFront();
-		    }
-		}
-		function resetHighlight(e) {
-		    geojson.resetStyle(e.target);
-		}
-		function zoomToFeature(e) {
-		    map.fitBounds(e.target.getBounds());
-		}
-
-		function onEachFeature(feature, layer) {
-		    layer.on({
-		        mouseover: highlightFeature,
-		        mouseout: resetHighlight,
-		        click: zoomToFeature
-		    });
-		}
 		geojson = L.geoJson(statesData, {
-		    style: style,
-		    onEachFeature: onEachFeature
+		    style: MapStyle(map).style,
+		    onEachFeature: MapStyle().onEachFeature
 		}).addTo(map);
 
+		ctr.pins(function(results, files){
+			results.data.map(function(point){
+				L.marker([ 
+					parseFloat(point[2]),
+					parseFloat(point[3])
+		 				])
+				.addTo(map);
+				return;
+			});
+		});
 
-		var legend = L.control({position: 'bottomright'});
+		ctr.heatpoints(function(results, files){
+			var heatpoints = results.data.map(function(point){
+				return[ parseFloat(point[2]),
+		 				parseFloat(point[3]),
+		 				parseFloat(point[4])
+		 				];
 
-		legend.onAdd = function (map) {
-
-		    var div = L.DomUtil.create('div', 'info legend'),
-		        grades = [0, 10, 20, 50, 100, 200, 500, 1000],
-		        labels = [];
-
-		    // loop through our density intervals and generate a label with a colored square for each interval
-		    for (var i = 0; i < grades.length; i++) {
-		        div.innerHTML +=
-		            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-		            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-		    }
-
-		    return div;
-		};
-
-		legend.addTo(map);
-
-		var info = L.control();
-
-		info.onAdd = function (map) {
-		    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
-		    this.update();
-		    return this._div;
-		};
-
-
-		info.update = function (props) {
-			console.log(props);
-		    // this._div.innerHTML = '<h2>Subprefeitura:'+props.name+'</h2>';
-		};
-
-		info.addTo(map);
-		var config = {
-			quotes: false,
-			download:true,
-			delimiter: ";",
-			header: false,
-			complete:function(results, file) {
-				var points = results.data;
-				var heatpoints = [];
-				var mks = [];
-			 	for (var p in points) {
-			 		var lat = parseFloat(points[p][2]),
-			 			lng = parseFloat(points[p][3]),
-			 			ind = parseFloat(points[p][4]),
-			 			hp = [lat, lng, ind];
-			 			mk = [lat, lng];
-			 			L.marker(mk).addTo(map);
-			 		heatpoints[p] = hp;
-			 		mks[p] = mk;
-			 	}
-			 	console.log(heatpoints)
-			 	a = heatpoints.slice(0,10)
-
-				var heatLayer = L.heatLayer(heatpoints, {radius: 35}).addTo(map);
-				//var markers = L.marker(mks).addTo(map);
-			},
-			newline: "\n"
-		}
-		var data = Papa.parse("csv/data_"+day+".csv",config);
-
-
-		var config2 = {
-			quotes: false,
-			download:true,
-			delimiter: ";",
-			header: false,
-			complete:function(results, file) {
-				var points = results.data;
-				var heatpoints = [];
-				var mks = [];
-			 	for (var p in points) {
-			 		var lat = parseFloat(points[p][2]),
-			 			lng = parseFloat(points[p][3]),
-			 			ind = parseFloat(points[p][4]),
-			 			hp = [lat, lng, ind];
-			 			mk = [lat, lng];
-			 			L.marker(mk).addTo(map);
-			 		mks[p] = mk;
-			 	}
-				var markers = L.marker(mks).addTo(map);
-			},
-			newline: "\n"
-		}
-		var data = Papa.parse("csv/data_20170102.csv",config);
-
+			});
+		 	var heatLayer = L.heatLayer(heatpoints, {radius: 30}).addTo(map);
+		});
 }
-
-
-var MapControl = (function(map){
-	var dayIdx = -1;
-	
-	return{
-		init: function(){
-			var dayIdx = -1;
-		},
-
-		next: function() {
-			return dates[++idx];
-		},
-		previous: function(){
-			return dates[--idx];
-		}
-
-	}
-
-	function init(){
-
-	}
-});
 
 var ctr = MapControl();
 
 var map = L.map('map', {center:[-23.543501,-46.507022], zoom: 12 });
-var idx = -1;
-function next(){
-	idx ++;
-	$("#day").html(dates[idx]);
-	return dates[idx];
-}
 
-function previous(){
-	idx --;
-	$("#day").html(dates[idx]);
-	return dates[idx];
-}
-render_map(map,next())
